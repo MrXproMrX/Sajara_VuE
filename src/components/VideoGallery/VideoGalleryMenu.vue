bf12488921d1f23c1d79b571bfeba1f596c91815
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2022-08-08 14:22:05 +0500

    видео узгартрилди

--- src/components/VideoGallery/VideoGalleryMenu.vue
+++ src/components/VideoGallery/VideoGalleryMenu.vue
@@ -1,5 +1,6 @@
 <template>
     <section>
+
         <div class="video__mrx2__list">
             <div class="video__mrx2__item" v-for="(video,index) in videos" :key="index" @click="$emit('video',video.videoList)">
                 <div class="video__mrx2__img" >
@@ -9,19 +10,14 @@
                 <h2 class="video__mrx2__title__h2">{{video.title}}</h2>
             </div>
         </div>
+
     </section>
 </template>
 
 <script>
-
-import VueVideoSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
 export default {
     emits:['video'],
     props:['videos'],
-    components:{
-        VueVideoSlickCarousel,
-    },
 }
 
 </script>
